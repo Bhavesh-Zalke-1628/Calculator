87f6c38b200e2e45d4ffb4d8d91d2458e55893b8
Bhavesh zalke <zalkebhavesh@gmail.com>
2023-08-16 08:31:50 +0530

    Files are added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function clr(){
+    document.getElementById('result').value = "";
+}
+function display(val){
+    document.getElementById('result').value+=val;
+}
+function equate(){
+    let x = document.getElementById('result').value ;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+}
